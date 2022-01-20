5c82d62c305caeb063501bd56daf5ade742a212b
David Regla <dreglad@gmail.com>
2022-01-20 11:12:17 -0600

    Upgrade landing to a standalone website

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,25 @@
 const path = require('path');
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: path.resolve(__dirname, 'dist', 'index.html'),
       hash: false,
-      filename: path.resolve(__dirname, 'dist', 'landing.html'),
     }),
     new CopyPlugin({
       patterns: [
-        {from: path.resolve(__dirname, 'assets'), to: path.resolve(__dirname, 'dist', 'assets')},
+        {
+          from: path.resolve(__dirname, 'src', 'assets'),
+          to: path.resolve(__dirname, 'dist', 'assets'),
+        },
       ],
     }),
   ],
